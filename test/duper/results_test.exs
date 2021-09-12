b35da1412af97d50947e50d25ac764939ba93746
big-meel <big.meel@zoho.com>
2021-09-12 01:53:12 -0500

    Add Supervisor for workers

--- test/duper/results_test.exs
+++ test/duper/results_test.exs
@@ -9,5 +9,12 @@ defmodule Duper.ResultsTest do
     Results.add_hash_for("path4", 789)
     Results.add_hash_for("path5", 456)
     Results.add_hash_for("path6", 999)
+
+    duplicates = Results.find_duplicates()
+
+    assert length(duplicates) == 2
+
+    assert ~w{path3 path1} in duplicates
+    assert ~w{path5 path2} in duplicates
   end
 end
